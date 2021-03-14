f570dee20ba800c6b3aedf7b9c0d92dfd23c9f6a
Pop <824173124@qq.com>
2021-03-14 19:18:27 +0800

    初始化

---
+++ src/main/java/com/pop/security/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.pop.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @program: security
+ * @description:
+ * @author: Pop
+ * @create: 2021-03-14 18:44
+ **/
+@Controller
+public class HelloController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
